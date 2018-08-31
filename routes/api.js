5e180b6a6579b2cf1dc9e3acf8d3225337c48ad6
Craig <craig.b.macgregor@gmail.com>
2018-09-01 01:12:09 +1200

    auth route configured

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var lodash = require('lodash');
 var config = require('config');
 var Client = require('bitcoin-core');
+var jwt = require('express-jwt');
+var fs = require('fs');
 
 var commands = [
   'getinfo',
@@ -16,6 +18,12 @@ var commands = [
   'backupwallet',
 ];
 
+//@TODO: remove data.body being parsed back
+//@TODO: hash the username and password
+//@TODO: enable https
+//@TODO: generate JWT token
+//@TODO: add JWT middleware on rpc route
+
 var settings = config.get('client');
 
 var navClient = new Client({
@@ -27,7 +35,53 @@ var navClient = new Client({
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Available endpoints - /rpc:POST');
+  res.send('Available endpoints [auth, rpc]');
+});
+
+router.post('/auth', function(req, res, next) {
+
+  if (!req.body || !req.body.username || !req.body.password){
+    var response = {
+      type: 'ERROR',
+      code: 'AUTH_001',
+      message: 'Invalid Request',
+      data: req.body,
+    }
+    res.send(JSON.stringify(response));
+    return
+  }
+
+  //check username and password
+  var auth = fs.readFileSync("./config/auth.json");
+  var authJson = JSON.parse(auth);
+
+  console.log('authJson', authJson);
+
+  if (authJson.username != req.body.username || authJson.password != req.body.password) {
+    var response = {
+      type: 'ERROR',
+      code: 'AUTH_002',
+      message: 'Invalid Username or Password',
+      data: req.body,
+    }
+    res.send(JSON.stringify(response));
+    return
+  }
+
+  var data = {
+    jwt: "test_token"
+  }
+
+  //check password is valid
+  var response = {
+    type: 'SUCCESS',
+    code: 'AUTH_002',
+    message: 'Successful Login',
+    data: data,
+  }
+  res.send(JSON.stringify(response));
+  return
+
 });
 
 router.post('/rpc', function(req, res, next) {
@@ -54,12 +108,12 @@ router.post('/rpc', function(req, res, next) {
   if (req.body.params) args = args.concat(req.body.params);
 
   //forward request to the navcoin cli
-  navClient.command(...args).then((response) => {
+  navClient.command(...args).then((data) => {
     var response = {
       type: 'SUCCESS',
       code: 'RPC_002',
       message: 'Successful Request',
-      data: response,
+      data: data,
     }
     res.send(JSON.stringify(response));
     return
