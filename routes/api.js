8145c244485b6ef7145a5cbb66e0352b3f54788c
Matt <mpau548@aucklanduni.ac.nz>
2019-02-20 12:04:51 +1300

    FIxed some syntax errors

--- routes/api.js
+++ routes/api.js
@@ -8,160 +8,162 @@ const fs = require('fs');
 const bcrypt = require('bcrypt');
 
 const RPC_COMMANDS = [
-  '￼getaddressbalance', 
-  'getaddressdeltas', 
-  'getaddressmempool', 
-  'getaddresstxids', 
-  'getaddressutxosgetbestblockhash', 
-  'getblock', 
-  'getblockchaininfo', 
-  'getblockcountgetblockhash', 
-  'getblockhashes', 
-  'getblockheader', 
-  'getchaintips', 
-  'getdifficulty', 
-  'getmempoolancestors', 
-  'getmempooldescendants', 
-  'getmempoolentry', 
-  'getmempoolinfo', 
-  'getrawmempool', 
-  'getspentinfo', 
-  'gettxout', 
-  'gettxoutproof', 
-  'gettxoutsetinfo', 
-  'verifychain', 
-  'verifytxoutproof', 
-  'createpaymentrequest', 
-  'createproposal', 
-  'donatefund', 
-  'getpaymentrequest', 
-  'getproposal', 
-  'listproposals', 
-  'paymentrequestvote', 
-  'paymentrequestvotelist', 
-  'proposalvote', 
-  'proposalvotelistgetinfo', 
-  'help', 
-  'stopgenerate', 
-  'generatetoaddress', 
-  'staking', 
-  'coinstakeinputs', 
-  'coinstakeoutputs', 
-  'forcetransactions', 
-  'setcoinbasestrdzeel', 
-  'setcoinstakestrdzeel', 
-  'getmininginfo', 
-  'getnetworkhashps', 
-  'prioritisetransaction', 
-  'submitblock', 
-  'addnode', 
-  'clearbanned', 
-  'disconnectnode', 
-  'getaddednodeinfo', 
-  'getconnectioncount', 
-  'getnettotals', 
-  'getnetworkinfo', 
-  'getpeerinfo', 
-  'getstakesubsidy', 
-  'getstakinginfo', 
-  'listbanned', 
-  'ping', 
-  'setban', 
-  'decoderawtransaction', 
-  'decodescript', 
-  'fundrawtransaction', 
-  'getrawtransaction', 
-  'sendrawtransaction', 
-  'signrawtransaction', 
-  'createwitnessaddress', 
-  'estimatefee', 
-  'estimatepriority', 
-  'estimatesmartfee', 
-  'estimatesmartpriority', 
-  'signmessagewithprivkey', 
-  'validateaddress', 
-  'verifymessage', 
-  'addmultisigaddress', 
-  'addwitnessaddress', 
-  'anonsend', 
-  'backupwallet', 
-  'dumpmasterprivkey', 
-  'dumpprivkey', 
-  'dumpwallet', 
-  'encryptwallet', 
-  'getaccount', 
-  'getaccountaddress', 
-  'getaddressesbyaccount', 
-  'getanondestination', 
-  'getbalance', 
-  'getcoldstakingaddress', 
-  'getnewaddress', 
-  'getrawchangeaddress', 
-  'getreceivedbyaccount', 
-  'getreceivedbyaddress', 
-  'getstakereport', 
-  'gettransaction', 
-  'getunconfirmedbalance', 
-  'getwalletinfo', 
-  'importaddress', 
-  'importprivkey', 
-  'importprunedfunds', 
-  'importpubkey', 
-  'importwallet', 
-  'keypoolrefill', 
-  'listaccounts', 
-  'listaddressgroupings', 
-  'listlockunspent', 
-  'listreceivedbyaccount', 
-  'listreceivedbyaddress', 
-  'listsinceblock', 
-  'listtransactions', 
-  'listunspent', 
-  'lockunspent', 
-  'move', 
-  'removeprunedfunds', 
-  'resolveopenallias', 
-  'sendfrom', 
-  'sendmany', 
-  'sendtoaddress', 
-  'setaccount', 
-  'settxfee', 
-  'signmessage', 
-  'stakervote', 
+  '￼getaddressbalance',
+  'getaddressdeltas',
+  'getaddressmempool',
+  'getaddresstxids',
+  'getaddressutxosgetbestblockhash',
+  'getblock',
+  'getblockchaininfo',
+  'getblockcountgetblockhash',
+  'getblockhashes',
+  'getblockheader',
+  'getchaintips',
+  'getdifficulty',
+  'getmempoolancestors',
+  'getmempooldescendants',
+  'getmempoolentry',
+  'getmempoolinfo',
+  'getrawmempool',
+  'getspentinfo',
+  'gettxout',
+  'gettxoutproof',
+  'gettxoutsetinfo',
+  'verifychain',
+  'verifytxoutproof',
+  'createpaymentrequest',
+  'createproposal',
+  'donatefund',
+  'getpaymentrequest',
+  'cfundstats',
+  'getproposal',
+  'listproposals',
+  'paymentrequestvote',
+  'paymentrequestvotelist',
+  'proposalvote',
+  'proposalvotelist',
+  'getinfo',
+  'help',
+  'stopgenerate',
+  'generatetoaddress',
+  'staking',
+  'coinstakeinputs',
+  'coinstakeoutputs',
+  'forcetransactions',
+  'setcoinbasestrdzeel',
+  'setcoinstakestrdzeel',
+  'getmininginfo',
+  'getnetworkhashps',
+  'prioritisetransaction',
+  'submitblock',
+  'addnode',
+  'clearbanned',
+  'disconnectnode',
+  'getaddednodeinfo',
+  'getconnectioncount',
+  'getnettotals',
+  'getnetworkinfo',
+  'getpeerinfo',
+  'getstakesubsidy',
+  'getstakinginfo',
+  'listbanned',
+  'ping',
+  'setban',
+  'decoderawtransaction',
+  'decodescript',
+  'fundrawtransaction',
+  'getrawtransaction',
+  'sendrawtransaction',
+  'signrawtransaction',
+  'createwitnessaddress',
+  'estimatefee',
+  'estimatepriority',
+  'estimatesmartfee',
+  'estimatesmartpriority',
+  'signmessagewithprivkey',
+  'validateaddress',
+  'verifymessage',
+  'addmultisigaddress',
+  'addwitnessaddress',
+  'anonsend',
+  'backupwallet',
+  'dumpmasterprivkey',
+  'dumpprivkey',
+  'dumpwallet',
+  'encryptwallet',
+  'getaccount',
+  'getaccountaddress',
+  'getaddressesbyaccount',
+  'getanondestination',
+  'getbalance',
+  'getcoldstakingaddress',
+  'getnewaddress',
+  'getrawchangeaddress',
+  'getreceivedbyaccount',
+  'getreceivedbyaddress',
+  'getstakereport',
+  'gettransaction',
+  'getunconfirmedbalance',
+  'getwalletinfo',
+  'importaddress',
+  'importprivkey',
+  'importprunedfunds',
+  'importpubkey',
+  'importwallet',
+  'keypoolrefill',
+  'listaccounts',
+  'listaddressgroupings',
+  'listlockunspent',
+  'listreceivedbyaccount',
+  'listreceivedbyaddress',
+  'listsinceblock',
+  'listtransactions',
+  'listunspent',
+  'lockunspent',
+  'move',
+  'removeprunedfunds',
+  'resolveopenallias',
+  'sendfrom',
+  'sendmany',
+  'sendtoaddress',
+  'setaccount',
+  'settxfee',
+  'signmessage',
+  'stakervote'
 ];
 
 //@TODO: remove data.body being parsed back
 
 const settings = config.get('client');
 
-console.log(settings)
+console.log(settings);
 const navClient = new Client({
   username: settings.navCoin.user,
   password: settings.navCoin.pass,
   port: settings.navCoin.port,
-  host: settings.navCoin.host,
-})
+  host: settings.navCoin.host
+});
 
 router.use(function(req, res, next) {
-
   //skip token middleware on auth attempt
-  if(req.originalUrl == '/api/auth'){
+  if (req.originalUrl == '/api/auth') {
     next();
-    return
+    return;
   }
 
-  const token = req.body.token || req.query.token || req.headers['x-access-token'];
+  const token =
+    req.body.token || req.query.token || req.headers['x-access-token'];
   if (token) {
-    fs.readFile('./config/auth.json', function(err, auth){
+    fs.readFile('./config/auth.json', function(err, auth) {
       if (err) {
         const response = {
           type: 'ERROR',
           code: 'JWT_001',
           message: 'Failed to read auth file from disk',
-          data: req.body,
-        }
+          data: req.body
+        };
         res.send(JSON.stringify(response));
-        return
+        return;
       }
       var authJson = JSON.parse(auth);
       // verifies secret and checks exp
@@ -171,28 +173,26 @@ router.use(function(req, res, next) {
             type: 'ERROR',
             code: 'JWT_002',
             message: 'Invalid Token',
-            data: req.body,
-          }
+            data: req.body
+          };
           res.send(JSON.stringify(response));
-          return
+          return;
         }
         // if everything is good, save to request for use in other routes
         console.log('TOKEN AUTHENTICATED');
         next();
-        return
+        return;
       });
     });
   } else {
-
     const response = {
       type: 'ERROR',
       code: 'JWT_002',
       message: 'No Token Provided',
-      data: req.body,
-    }
+      data: req.body
+    };
     res.send(JSON.stringify(response));
-    return
-
+    return;
   }
 });
 
@@ -202,40 +202,41 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/auth', function(req, res, next) {
-
   //check username and password
   var auth = fs.readFileSync('./config/auth.json');
   var authJson = JSON.parse(auth);
 
-  if (!req.body || !req.body.username || !req.body.password){
+  if (!req.body || !req.body.username || !req.body.password) {
     const response = {
       type: 'ERROR',
       code: 'AUTH_001',
       message: 'Invalid Request',
-      data: req.body,
-    }
+      data: req.body
+    };
     res.send(JSON.stringify(response));
-    return
+    return;
   }
 
-  if (!bcrypt.compareSync(req.body.username, authJson.username) || !bcrypt.compareSync(req.body.password, authJson.password)) {
+  if (
+    !bcrypt.compareSync(req.body.username, authJson.username) ||
+    !bcrypt.compareSync(req.body.password, authJson.password)
+  ) {
     var response = {
-
       type: 'ERROR',
       code: 'AUTH_002',
       message: 'Invalid Username or Password',
-      data: req.body,
-    }
+      data: req.body
+    };
     res.send(JSON.stringify(response));
-    return
+    return;
   }
 
   const data = {
-    user: req.body.username,
-  }
+    user: req.body.username
+  };
 
   const token = jwt.sign(data, authJson.secret, {
-    expiresIn: 60*60*24,
+    expiresIn: 60 * 60 * 24
   });
 
   //check password is valid
@@ -244,55 +245,61 @@ router.post('/auth', function(req, res, next) {
     code: 'AUTH_002',
     message: 'Successful Login',
     data: data,
-    token: token,
-  }
+    token: token
+  };
 
   res.send(JSON.stringify(response));
-  return
-
+  return;
 });
 
 router.post('/rpc', function(req, res, next) {
-  console.log(`'/rpc called: ${req.body.command}`)
+  console.log(`'/rpc called: ${req.body.command}`);
   //check if command on allowed list
-  if (!req.body || !req.body.command || RPC_COMMANDS.indexOf(req.body.command) == -1){
+  if (
+    !req.body ||
+    !req.body.command ||
+    RPC_COMMANDS.indexOf(req.body.command) == -1
+  ) {
     const response = {
       type: 'ERROR',
       code: 'RPC_001',
       message: 'Invalid Request',
-      data: req.body,
-    }
+      data: req.body
+    };
     res.send(JSON.stringify(response));
-    return
+    return;
   }
 
   let args = [req.body.command];
 
   //add params if they exist
   if (req.body.params) args = args.concat(req.body.params);
-  
-  console.log('Args:', args)
+
+  console.log('Args:', args);
   //forward request to the navcoin cli
-  navClient.command(...args).then((data) => {
-    const response = {
-      type: 'SUCCESS',
-      code: 'RPC_002',
-      message: 'Successful Request',
-      data: data,
-    }
-    console.log('success')
-    res.send(JSON.stringify(response));
-    return
-  }).catch((err) => {
-    const response = {
-      type: 'ERROR',
-      code: 'RPC_003',
-      message: 'RPC Error',
-      data: {'error': err.code, 'message': err.message},
-    }
-    res.send(JSON.stringify(response));
-    return
-  });
+  navClient
+    .command(...args)
+    .then(data => {
+      const response = {
+        type: 'SUCCESS',
+        code: 'RPC_002',
+        message: 'Successful Request',
+        data: data
+      };
+      console.log('success');
+      res.send(JSON.stringify(response));
+      return;
+    })
+    .catch(err => {
+      const response = {
+        type: 'ERROR',
+        code: 'RPC_003',
+        message: 'RPC Error',
+        data: { error: err.code, message: err.message }
+      };
+      res.send(JSON.stringify(response));
+      return;
+    });
 });
 
 router.post('/walletoverview', (req, res, next) => {
@@ -307,109 +314,123 @@ router.post('/walletoverview', (req, res, next) => {
 
   const batch = [
     { method: 'getblockchaininfo' },
-    { method: 'getstakinginfo'},
-    { method: 'getinfo' },
-  ]
-  navClient.command(batch).then((responses) => {
-    console.log(responses)
+    { method: 'getstakinginfo' },
+    { method: 'getinfo' }
+  ];
+  navClient
+    .command(batch)
+    .then(responses => {
+      console.log(responses);
 
-    const getBlockchainInfoData = responses[0];
-    const getStakingInfoData = responses[1];
-    const getInfoData = responses[2];
+      const getBlockchainInfoData = responses[0];
+      const getStakingInfoData = responses[1];
+      const getInfoData = responses[2];
 
-    const response = {
-      type: 'SUCCESS',
-      code: 'RPC_002',
-      message: 'Successful Request',
-      data: {
-        walletVersion: getInfoData.version,
-        walletChain: getBlockchainInfoData.chain,
-        isLocked: (getInfoData.unlocked_until > 0),
-        isStaking: getStakingInfoData.staking,
-        currentBlock: getBlockchainInfoData.blocks,
-        highestKnownBlock: getBlockchainInfoData.blocks, //TODO Verify this is correct
-        isSyncing: (this.currentBlock < this.highestKnownBlock ), //TODO Verify this is correct
-      },
-    }
-    console.log('success')
-    res.send(JSON.stringify(response));
-    return
+      const response = {
+        type: 'SUCCESS',
+        code: 'RPC_002',
+        message: 'Successful Request',
+        data: {
+          walletVersion: getInfoData.version,
+          walletChain: getBlockchainInfoData.chain,
+          isLocked: getInfoData.unlocked_until > 0,
+          isStaking: getStakingInfoData.staking,
+          currentBlock: getBlockchainInfoData.blocks,
+          highestKnownBlock: getBlockchainInfoData.blocks, //TODO Verify this is correct
+          isSyncing: this.currentBlock < this.highestKnownBlock //TODO Verify this is correct
+        }
+      };
+      console.log('success');
+      res.send(JSON.stringify(response));
+      return;
+    })
+    .catch(err => {
+      const response = {
+        type: 'ERROR',
+        code: 'RPC_003',
+        message: 'RPC Error',
+        data: { error: err.code, message: err.message }
+      };
+      res.send(JSON.stringify(response));
+      return;
+    });
+});
 
-  }).catch((err) => {
-    const response = {
-      type: 'ERROR',
-      code: 'RPC_003',
-      message: 'RPC Error',
-      data: {'error': err.code, 'message': err.message},
-    }
-    res.send(JSON.stringify(response));
-    return
-  }
- router.post('/ui-password', function(req, res, next) {
-
+router.post('/ui-password', function(req, res, next) {
   //check if command on allowed list
-  if (!req.body || !req.body.username || !req.body.password || !req.body.currentUsername || !req.body.currentPassword){
+  if (
+    !req.body ||
+    !req.body.username ||
+    !req.body.password ||
+    !req.body.currentUsername ||
+    !req.body.currentPassword
+  ) {
     var response = {
       type: 'ERROR',
       code: 'UIPASS_001',
       message: 'Invalid Request',
-      data: req.body,
-    }
+      data: req.body
+    };
     res.send(JSON.stringify(response));
-    return
+    return;
   }
 
-  fs.readFile('./config/auth.json', function(err, auth){
-    if(err){
+  fs.readFile('./config/auth.json', function(err, auth) {
+    if (err) {
       var response = {
         type: 'ERROR',
         code: 'UIPASS_002',
         message: 'Failed to read auth file from disk',
-        data: req.body,
-      }
+        data: req.body
+      };
       res.send(JSON.stringify(response));
-      return
+      return;
     }
     var authJson = JSON.parse(auth);
 
-    if (!bcrypt.compareSync(req.body.currentUsername, authJson.username) || !bcrypt.compareSync(req.body.currentPassword, authJson.password)) {
+    if (
+      !bcrypt.compareSync(req.body.currentUsername, authJson.username) ||
+      !bcrypt.compareSync(req.body.currentPassword, authJson.password)
+    ) {
       var response = {
         type: 'ERROR',
         code: 'AUTH_002',
         message: 'Invalid Username or Password',
-        data: req.body,
-      }
+        data: req.body
+      };
       res.send(JSON.stringify(response));
-      return
+      return;
     }
 
     authJson.username = bcrypt.hashSync(req.body.username, 10);
     authJson.password = bcrypt.hashSync(req.body.password, 10);
 
-    fs.writeFile('./config/auth.json', JSON.stringify(authJson), 'utf8', function(err){
-      if(err){
-        var response = {
-          type: 'ERROR',
-          code: 'UIPASS_003',
-          message: 'Failed to write to disk',
-          data: req.body,
+    fs.writeFile(
+      './config/auth.json',
+      JSON.stringify(authJson),
+      'utf8',
+      function(err) {
+        if (err) {
+          var response = {
+            type: 'ERROR',
+            code: 'UIPASS_003',
+            message: 'Failed to write to disk',
+            data: req.body
+          };
+          res.send(JSON.stringify(response));
+          return;
         }
+        var response = {
+          type: 'SUCCESS',
+          code: 'UIPASS_002',
+          message: 'Successful Request',
+          data: 'Password Updated'
+        };
         res.send(JSON.stringify(response));
-        return
+        return;
       }
-      var response = {
-        type: 'SUCCESS',
-        code: 'UIPASS_002',
-        message: 'Successful Request',
-        data: 'Password Updated',
-      }
-      res.send(JSON.stringify(response));
-      return
-    });
-
+    );
   });
-
 });
 
 module.exports = router;
-
