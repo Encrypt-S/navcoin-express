9723b2734a8bb6b2b5e16d78f24e6b5ce801a4a0
Craig <craig.b.macgregor@gmail.com>
2019-03-19 15:51:09 +1300

    updated startup script

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -3,7 +3,13 @@
 installed=$(find /home/odroid/navdroid -type d -maxdepth 1 -name 'navcoin-*')
 installed="${installed:30}"
 
+/home/odroid/navdroid/navcoin-${installed}/bin/navcoin-cli stop
 /home/odroid/navdroid/navcoin-${installed}/bin/navcoind &
 
-forever start -c "npm start" /home/odroid/navdroid/angular
-forever start -c "npm start" /home/odroid/navdroid/express
+forever stopall
+
+cd /home/odroid/navdroid/express
+forever start ./bin/www
+
+cd /home/odroid/navdroid/angular
+forever start node_modules/@angular/cli/bin/ng serve --ssl true --proxy-config proxy.config.json --host 0.0.0.0
