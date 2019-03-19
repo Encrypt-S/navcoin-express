b8e66c27a54a2d16df29bc17956ad4a2adde46d7
Craig <craig.b.macgregor@gmail.com>
2019-03-19 14:23:01 +1300

    running daemon from local dir

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-/usr/local/bin/navcoind &
+installed=$(find /home/odroid/navdroid -name 'navcoin-*' -type d -maxdepth 1)
+installed="${installed:30}"
+
+/home/odroid/navdroid/navcoin-${installed}/bin/navcoind &
+
 forever start -c "npm start" /home/odroid/navdroid/angular
 forever start -c "npm start" /home/odroid/navdroid/express
--- scripts/update-daemon.sh
+++ scripts/update-daemon.sh
@@ -8,7 +8,6 @@ now=$(date +"%m-%d-%Y %T")
 
 touch /home/odroid/navdroid/express/log/update.json
 chmod 777 /home/odroid/navdroid/express/log/update.json
-chown pi:www-data /home/odroid/navdroid/express/log/update.json
 latest=$(curl https://api.github.com/repos/NAVCoin/navcoin-core/releases/latest)
 
 next=false
@@ -37,8 +36,8 @@ fi
 
 echo "Latest Version: $tag"
 
-installed=$(find /home/odroid/navdroid -name 'navcoin-*' -type d)
-installed="${installed:23}"
+installed=$(find /home/odroid/navdroid -name 'navcoin-*' -type d -maxdepth 1)
+installed="${installed:30}"
 
 echo "Installed Version: $installed"
 
@@ -65,16 +64,13 @@ fi
 
 echo "Removing old files"
 rm -rf /home/odroid/navdroid/navcoin-${installed}
-rm -rf /usr/local/bin/navcoind
 
 echo "Expanding new version"
 tar zxvf navcoin-${tag}-arm-linux-gnueabihf.tar.gz -C /home/odroid/navdroid/
 rm navcoin-${tag}-arm-linux-gnueabihf.tar.gz
 
-echo "Copying to the startup location"
-cp /home/odroid/navdroid/navcoin-${tag}/bin/navcoind /usr/local/bin/navcoind
-chmod +x /usr/local/bin/navcoind
+echo "Restarting navcoin daemon"
 /home/odroid/navdroid/navcoin-${tag}/bin/navcoin-cli stop
-/usr/local/bin/navcoind &
-echo "NavCoin has Successfully updated to the latest version"
+/home/odroid/navdroid/navcoin-${tag}/bin/navcoind &
+echo "NavCoin has successfully updated to the latest version"
 echo "{\"last_run\":\"${now}\",\"success\":true,\"code\":\"UPDATE_INSTALLED\"}" > /home/odroid/navdroid/express/log/update.json
