e9feb58813c15e0223284b7259bef20acb7e4859
Craig <craig.b.macgregor@gmail.com>
2019-03-21 12:19:10 +1300

    adding timestamp to logs

--- scripts/update-ui.sh
+++ scripts/update-ui.sh
@@ -4,30 +4,32 @@ log=/home/odroid/navdroid/express/log/update-ui.log
 touch $log
 chmod 777 $log
 
+now=$(date +"%m-%d-%Y %T")
+
 forever stopall >> $log
 
-echo "forever stopped" >> $log
+echo "${now} - forever stopped" >> $log
 
 cd /home/odroid/navdroid/angular
 git pull >> $log
 # npm install
 
-echo "pulled angular" >> $log
+echo "${now} - pulled angular" >> $log
 
 cd /home/odroid/navdroid/express
 git pull >> $log
 # npm install
 
-echo "pulled express" >> $log
+echo "${now} - pulled express" >> $log
 
-echo "attempting to restart express" >> $log
+echo "${now} - attempting to restart express" >> $log
 
 cd /home/odroid/navdroid/express
 forever start ./bin/www >> $log
 
-echo "attempting to restart angular" >> $log
+echo "${now} - attempting to restart angular" >> $log
 
 cd /home/odroid/navdroid/angular
 forever start node_modules/@angular/cli/bin/ng serve --ssl true --proxy-config proxy.config.json --host 0.0.0.0  >> $log
 
-echo "script finished" >> $log
+echo "${now} - script finished" >> $log
