10255680054330dbfa90b43e9581c500139a66aa
Craig <craig.b.macgregor@gmail.com>
2019-03-21 12:18:12 +1300

    adding logging back in

--- scripts/update-ui.sh
+++ scripts/update-ui.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
-forever stopall
+log=/home/odroid/navdroid/express/log/update-ui.log
+touch $log
+chmod 777 $log
+
+forever stopall >> $log
+
+echo "forever stopped" >> $log
 
 cd /home/odroid/navdroid/angular
-git pull
+git pull >> $log
 # npm install
 
-cd /home/odroid/navdroid/express
-git pull
-# npm install
+echo "pulled angular" >> $log
 
 cd /home/odroid/navdroid/express
-forever start ./bin/www
+git pull >> $log
+# npm install
+
+echo "pulled express" >> $log
+
+echo "attempting to restart express" >> $log
+
+cd /home/odroid/navdroid/express
+forever start ./bin/www >> $log
+
+echo "attempting to restart angular" >> $log
 
 cd /home/odroid/navdroid/angular
-forever start node_modules/@angular/cli/bin/ng serve --ssl true --proxy-config proxy.config.json --host 0.0.0.0
+forever start node_modules/@angular/cli/bin/ng serve --ssl true --proxy-config proxy.config.json --host 0.0.0.0  >> $log
+
+echo "script finished" >> $log
