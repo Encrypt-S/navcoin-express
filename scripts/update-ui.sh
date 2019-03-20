98b61adc5753a8b84ad52cb952c28bb952fff040
Craig <craig.b.macgregor@gmail.com>
2019-03-21 12:24:24 +1300

    minimal logs

--- scripts/update-ui.sh
+++ scripts/update-ui.sh
@@ -6,30 +6,20 @@ chmod 777 $log
 
 now=$(date +"%m-%d-%Y %T")
 
-forever stopall >> $log
+echo "${now} - updating ui" >> $log
 
-echo "${now} - forever stopped" >> $log
+forever stopall >> $log
 
 cd /home/odroid/navdroid/angular
 git pull >> $log
 # npm install
 
-echo "${now} - pulled angular" >> $log
-
 cd /home/odroid/navdroid/express
 git pull >> $log
 # npm install
-
-echo "${now} - pulled express" >> $log
-
-echo "${now} - attempting to restart express" >> $log
 
 cd /home/odroid/navdroid/express
 forever start ./bin/www >> $log
 
-echo "${now} - attempting to restart angular" >> $log
-
 cd /home/odroid/navdroid/angular
 forever start node_modules/@angular/cli/bin/ng serve --ssl true --proxy-config proxy.config.json --host 0.0.0.0  >> $log
-
-echo "${now} - script finished" >> $log
