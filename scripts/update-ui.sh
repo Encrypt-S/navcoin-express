6a56b0dea93a8534c654c193f4165a865af1cf95
Craig <craig.b.macgregor@gmail.com>
2019-03-21 10:33:20 +1300

    added logging

--- scripts/update-ui.sh
+++ scripts/update-ui.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
-forever stopall
+log=/home/odroid/navdroid/express/log/update-ui.log
+touch $log
+chmod 777 $log
+
+forever stopall >> $log
+
+echo "forever stopped \n" >> $log
 
 cd /home/odroid/navdroid/angular
-git pull
+git pull >> $log
 # npm install
+
+echo "pulled angular \n" >> $log
 
 cd /home/odroid/navdroid/express
-git pull
+git pull >> $log
 # npm install
 
-sudo reboot -f
+echo "pulled express \n" >> $log
+
+echo "rebooting \n" >> $log
+
+sudo reboot -f >> $log
+
+echo "rebooted \n" >> $log
+
 
 # cd /home/odroid/navdroid/express
 # forever start ./bin/www
