aa43d08fbec85f60b0018df126f27f817a6af8ca
Craig <craig.b.macgregor@gmail.com>
2019-03-21 12:25:20 +1300

    reboot script logging

--- scripts/reboot.sh
+++ scripts/reboot.sh
@@ -4,15 +4,10 @@ log=/home/odroid/navdroid/express/log/reboot.log
 touch $log
 chmod 777 $log
 
-echo "attempting shutdown" >> $log
+now=$(date +"%m-%d-%Y %T")
 
-sudo /sbin/shutdown -r now >> $log
+echo "${now} - rebooting" >> $log
+
+sudo /sbin/reboot -f >> $log
 
 echo "script finished" >> $log
-
-
-# cd /home/odroid/navdroid/express
-# forever start ./bin/www
-
-# cd /home/odroid/navdroid/angular
-# forever start node_modules/@angular/cli/bin/ng serve --ssl true --proxy-config proxy.config.json --host 0.0.0.0
