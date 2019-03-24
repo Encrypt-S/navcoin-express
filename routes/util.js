cf23362781846d84f5bc4a4c1a75227ccf4b17fa
Craig <craig.b.macgregor@gmail.com>
2019-03-25 12:10:42 +1300

    update test

--- routes/util.js
+++ routes/util.js
@@ -78,7 +78,7 @@ router.post('/update-daemon', (req, res, next) => {
   var verified = common.checkPassword(req.body.password);
   if (!verified) {
     const response = JSON.stringify(
-      generateResponseObject('ERROR', 'PASSWD_001', 'Unauthorized', {})
+      generateResponseObject('ERROR', 'PASSWD_001', 'Unauthorized', { test: 'TEST_001'})
     );
     res.status(500).send(response);
     return;
