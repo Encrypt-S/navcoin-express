130b1e499b2a0c5bb9beb9d7e9d9c9c9465b7db9
Craig <craig.b.macgregor@gmail.com>
2019-03-27 12:06:42 +1300

    import script

--- scripts/import.sh
+++ scripts/import.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 echo "IMPORTING"
 
+log=/home/odroid/navdroid/express/log/import-wallet.log
+touch $log
+chmod 777 $log
+
+now=$(date +"%m-%d-%Y %T")
+echo "${now} - importing wallet" >> $log
+
 FILE=$1
 
-echo $FILE
+mv /home/odroid/.navcoin4/wallet.dat /home/odroid/.navcoin4/${date}_wallet.dat
+
+cp $FILE /home/odroid/.navcoin4/wallet.dat
 
 exit 0
