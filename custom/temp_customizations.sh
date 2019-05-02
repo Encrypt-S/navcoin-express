d35e19d3ffab73fc0701765da2275e192fcf66a9
mntyfrsh <ryan@prosoundsystem.com>
2019-05-01 21:05:25 -0400

    fs_resize.sh = script to expand microsd filesystem >16GB. This script points to a static variable and may cause problems. Suggested rewrite to dynamically detect devices based on combination of size + fs type + partition id.
    temp_customizations.sh = commands used to customize os from base
    install.

---
+++ custom/temp_customizations.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+## Temporary
+##
+## OS customizations
+#
+
+# install packages
+apt-get install sysstat # monitor with iostat
+
+# fix date/time with ntpdate
+service ntp stop && ntpdate -u time.nist.gov
+service ntp start
+
+# enable ssh
+ufw allow ssh
+
+# set vim as default editor
+update-alternatives --set editor /usr/bin/vim.basic
+
+# expand microsd filesystem on next boot
+# this script points to a static drive variable and may break at some point
+chmod +x fs_resize.sh
+bash -x fs_resize.sh
