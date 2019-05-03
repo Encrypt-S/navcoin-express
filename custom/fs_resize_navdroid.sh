347a62f6874d4c18ae450fd0033f47044e7e8ff0
mntyfrsh <ryan@prosoundsystem.com>
2019-05-03 03:33:19 -0400

    1. Updated checkinstall command
    2. Updated filesystem resize script

--- custom/fs_resize_navdroid.sh
+++ custom/fs_resize_navdroid.sh
@@ -1,36 +1,5 @@
 #!/bin/bash
 
-fs_resize() {
-
-        AC=$(whiptail --backtitle "Hardkernel ODROID Utility v$_REV" --yesno "Before running this, you must be sure that /dev/mmcblk1p2 is your root partition.
-                To do this make sure you are booting the eMMC or the microSD ALONE!.
-
-                DO YOU WANT TO CONTINUE?" 0 0 3>&1 1>&2 2>&3)
-
-        rets=$?
-        if [ $rets -eq 0 ]; then
-                do_resize
-        elif [ $rets -eq 1 ]; then
-                return 0
-        fi
-
-        return 0
-}
-
-do_resize() {
-        case "$DISTRO" in
-                "ubuntu")
-                        resize_p2 ;;
-                "debian")
-                        resize_p2 ;;
-                "ubuntu-server")
-                        resize_p2 ;;
-                *)
-                        msgbox "FS_RESIZE: Sorry your distro $DISTRO isn't supported yet. Please report on the forums"
-                        ;;
-        esac
-}
-
 resize_p2() {
         # this takes in consideration /dev/mmcblk1p2 as the rootfs!
         sleep 4
--- custom/temp_customizations.sh
+++ custom/temp_customizations.sh
@@ -27,6 +27,7 @@ apt -y autoremove
 
 # install packages
 apt -y install build-essential libcurl3-dev libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libqrencode-dev libboost-all-dev libminiupnpc-dev synaptic htop libunbound-dev libzmq3-dev zram-config git libdb4.8-dev libdb4.8++-dev ntp ntpdate sysstat screen checkinstall nginx vim
+apt -y --fix-broken install
 
 # set vim as default editor
 update-alternatives --set editor /usr/bin/vim.basic
@@ -44,8 +45,8 @@ update-alternatives --set editor /usr/bin/vim.basic
 
 # expand microsd filesystem on next boot
 # this script points to a static drive variable and may break at some point
-chmod +x fs_resize.sh
-bash -x fs_resize.sh
+#chmod +x fs_resize_navdroid.sh
+#bash -x fs_resize_navdroid.sh
 
 # clear bash history
 #history -c
@@ -53,18 +54,16 @@ cat /dev/null > ~/.bash_history
 
 
 
-# install navcoind
+# install navcoin-core
 cd /home/odroid
 git clone https://github.com/navcoin/navcoin-core.git
 cd navcoin-core
 ./autogen.sh
-./configure CFLAGS="-O2 -mtune=cortex-a15.cortex-a7 -mfpu=neon" CXXFLAGS="-O2 -mtune=cortex-a15.cortex-a7 -mfpu=neon" --enable-hardening --without-gui
+./configure CFLAGS="-O2 -mtune=cortex-a15.cortex-a7 -mfpu=neon" CXXFLAGS="-O2 -mtune=cortex-a15.cortex-a7 -mfpu=neon" --enable-hardening --without-gui --enable-upnp-default
 make -j3
-make install
 
-# alternate
-./configure CFLAGS="-O2 -mtune=cortex-a15.cortex-a7 -mfpu=neon" CXXFLAGS="-O2 -mtune=cortex-a15.cortex-a7 -mfpu=neon" --enable-hardening --without-gui --enable-upnp-default --prefix=/opt
-make -j3
-checkinstall
+# checkinstall to generate dpkg
+checkinstall -D -y --maintainer "info@navcoin.org" --pkgname navcoin-core --pkgversion 4.5.2 --requires nginx,build-essential,libcurl3-dev,libtool,autotools-dev,automake,pkg-config,zram-config,git,ntp
+
+# clean up
 make clean
-
